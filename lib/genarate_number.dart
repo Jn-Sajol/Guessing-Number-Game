507f8288f0d8d0eefd1560a60411d612247202c5
Jn-Sajol <julkar10121@gmail.com>
2024-03-04 21:13:02 +0600

    adding genarate random number

--- lib/genarate_number.dart
+++ lib/genarate_number.dart
@@ -1,48 +1,70 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:guessnumber/dividing.dart';
+import 'package:guessnumber/provider/provider.dart';
+import 'package:provider_base_tools/provider_base_tools.dart';
+
 class GenarateNumberPage extends StatelessWidget {
   const GenarateNumberPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Random random = Random();
+    // Generate a random number between 1 and 10000
+    int randomNumber = random.nextInt(10000) + 1;
+
+    final res = Provider.of<Result>(context);
     return Scaffold(
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25),
-          child: Center(
-            child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            
-              children: [
-                const Text('Hey lending',style: TextStyle(fontSize: 34,color: Colors.white,fontWeight: FontWeight.bold),),
-                const SizedBox(height: 23,),
-                const Text('Please Enter Your Name',style: TextStyle(fontSize: 24,color: Colors.white)),
-                const SizedBox(height: 23,),
-                // const TextField(
-                // decoration: InputDecoration(
-                // filled: true,
-                // fillColor: Color.fromARGB(73, 251, 251, 251),
-                // border: OutlineInputBorder(
-                // borderRadius: BorderRadius.all(Radius.circular(13))
-                // )
-                // ),
-                // ),
-                //  const SizedBox(height: 23,),
-                ElevatedButton(
-                style: const  ButtonStyle(
-                padding: MaterialStatePropertyAll(EdgeInsets.all(20)),
-                  // shape: MaterialStateProperty.all(OutlinedBorder()),
-                ),
-                  onPressed: () {
-                   Navigator.push(context, 
-                MaterialPageRoute(builder: (context)=> const DividingPage())
-                );
-                  },
-                  child: const Text("Next",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),),
-                )
-              ],
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+
+          children: [
+             Text(
+              'Now, I am Giving You $randomNumber . Now you have to Add your previous additional Result Number',
+              style: const TextStyle(
+                  fontSize: 34,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
             ),
-          ),
-        ));
+            const SizedBox(
+              height: 23,
+            ),
+             Text('Hints: Supppose You Have 6 , So now you should add (+) with $randomNumber',
+                style: const TextStyle(fontSize: 10, color: Colors.white)),
+            const SizedBox(
+              height: 23,
+            ),
+            const Text('After Adding Number Please Press Next Button.',
+                style: TextStyle(fontSize: 14, color: Color.fromARGB(255, 122, 108, 108))),
+            const SizedBox(
+              height: 23,
+            ),
+            ElevatedButton(
+              style: const ButtonStyle(
+                padding: MaterialStatePropertyAll(EdgeInsets.all(20)),
+                // shape: MaterialStateProperty.all(OutlinedBorder()),
+              ),
+              onPressed: () {
+              res.result = randomNumber;
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const DividingPage()));
+              },
+              child: const Text(
+                "Next",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
